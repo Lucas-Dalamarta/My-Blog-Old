3fa35db03cc15f47007fe1cf0fad99588f632fbb
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-20 23:17:07 -0300

    Created Profile component and inserted it into layout, also created AboutPage

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Lucas Felipe Dalamarta</h1>
+    <h2>Desenvolvedor Backend</h2>
+    <p>Um jovem desenvolvedor Backend, entusiasta em Frontend</p>
+  </div>
+)
+
+export default Profile
