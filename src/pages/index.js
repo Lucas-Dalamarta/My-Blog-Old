420c361ece9c4c46e4dcc160df65d07f763c0de1
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-23 01:55:12 -0300

    Created PosItem Component and added a dummy test

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from '../components/PostItem'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="1850"
+      timeToRead="55"
+      title="New city founded: Hill Valley!"
+      description="Hill Valley, California, was the hometown of the McFly family, the Brown family, the Tannen family, as well as several thousands more including the Strickland family. The city has three high schools: Hill Valley High School, Clayton High School, and Calahan High School; and a community college."
+    />
   </Layout>
 )
 
