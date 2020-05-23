a08e337d7bad18d96aae5c480b34d80f55a797de
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-22 23:37:56 -0300

    Created SocialLinks component and added it to Sidebar

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+const handle = "Lucas-Dalamarta"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: "Codepen",
+    url: `https://codepen.io/${handle}`
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/${handle}`
+  }
+]
+
+export default links
