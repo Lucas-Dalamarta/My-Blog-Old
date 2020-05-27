2bb1e96ce839bc46ddae134df0130f98ee5f63ee
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-27 00:20:48 -0300

    Added and config prismJs

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,8 @@ module.exports = {
               linkImagesToOriginal: false
             }
           },
-          `gatsby-remark-lazy-load`
+          `gatsby-remark-lazy-load`,
+          `gatsby-remark-prismjs`
         ],
       },
     },
