04f2c97b2a5873f533525f66b00e6c4687b265df
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-27 00:28:53 -0300

    Added next and previous requests

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,34 +24,52 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
-    {
-      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-        edges {
-          node {
-            frontmatter {
-              background
-              category
-              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-              description
-              title
-            }
-            timeToRead
-            fields {
-              slug
-            }
+  {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+          fields {
+            slug
+          }
+        }
+        next {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+        previous {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
           }
         }
       }
     }
+  }
   `).then(result => {
     const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(({node}) => {
+    posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/blog-post.js'),
         context: {
-          slug: node.fields.slug
+          slug: node.fields.slug,
+          previousPost : next,
+          nextPost: previous
         }
       })
     })
