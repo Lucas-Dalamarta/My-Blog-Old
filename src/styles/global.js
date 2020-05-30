311f9984299cf2ecff8b1e75e036c3ba3554c808
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-30 18:26:16 -0300

    Fixed order of prop

--- src/styles/global.js
+++ src/styles/global.js
@@ -74,8 +74,8 @@ const GlobalStyles = createGlobalStyle`
   }
   body.light {
     --borders: #dedede;
-    --postColor: #111;
     --texts: #555555;
+    --postColor: #111;
     --highlight: #1fa1f2;
     --mediumBackground: #f0f0f3;
     --background: #fff;
