bd4fd6962d8a1c5c30bce7fa15004756557e88fb
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-30 20:08:59 -0300

    Adding Public folder

---
+++ public/component---src-pages-about-js-556222ad513d91dd6fc5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),l=n.n(a),r=n("Zttt"),u=n("vrFN");e.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"About"}),l.a.createElement("h1",null,"TODO "))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-556222ad513d91dd6fc5.js.map
