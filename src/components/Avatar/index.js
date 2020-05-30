ba5f5fac6c756695d5ea0fdb9a021a31a377669b
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-30 16:10:09 -0300

    Added resposive queries

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-import * as S from './styled'
+import * as S from "./styled"
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery (
+  const { avatarImage } = useStaticQuery(
     graphql`
-    query {
-      avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
-        childImageSharp {
-          fixed(width: 60, height: 60) {
-            ...GatsbyImageSharpFixed
+      query {
+        avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 60) {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
-    }
     `
   )
 
-  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
+  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fluid} />
 }
-
 
 export default Avatar
