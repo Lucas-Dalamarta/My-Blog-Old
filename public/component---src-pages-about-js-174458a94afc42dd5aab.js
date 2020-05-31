04737cbcdf4092ee1b090c5eebebf2f380ab9d55
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-30 22:10:25 -0300

    Deleting files

--- public/component---src-pages-about-js-174458a94afc42dd5aab.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),l=n.n(a),r=n("Zttt"),u=n("vrFN");e.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"About"}),l.a.createElement("h1",null,"TODO "))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-174458a94afc42dd5aab.js.map
--- public/styles-0088909554f7ad2ac065.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],[]]);
-//# sourceMappingURL=styles-0088909554f7ad2ac065.js.map
