e2385309e936a301a723c3620a41369272433da7
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-05-31 01:49:53 -0300

    Hope this is the last one

---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,11 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Codepen } from "@styled-icons/boxicons-logos/Codepen"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+
+const Icons = {
+  Github,
+  Codepen,
+  Linkedin,
+}
+
+export default Icons
